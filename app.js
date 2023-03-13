60803e29d84c3edf920d2690ce11cf41fa4fe0ae
CouchMeka <meka.daniels1@gmail.com>
2023-03-13 11:44:37 -0400

    gitignore

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express()
+
+app.get('/', (req, res) => res.send("Hello World!"));
+
+//sets the port 
+const port = process.env.PORT || 8000
+
+//runs the application on port 8000
+app.listen(port, () => console.log(`server running on port ${port}`))
